70261822fd8f51b9dc17478ebaec90e9e66d5fbb
dianannperla <waafirmelyza@hotmail.com>
2025-09-13 15:53:09 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Weather from './components/Weather';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ứng dụng Thời tiết</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
